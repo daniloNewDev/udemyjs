566eb01f83ecb3f7fd6440d3c667c4d8a8489f9d
Danilo Leite <danilo.cebolino@gmail.com>
2023-11-10 20:16:16 -0300

    functions

--- 7-incrementacao/script.js
+++ 7-incrementacao/script.js
@@ -2,19 +2,11 @@ let numero=[1,2,3,4,5,6,7,8]
 for (let indice=0;indice<=7;indice++) {
     console.log(numero[indice])
 }
-
-
-const arr=[1,2,3,4,5,6,7,8]
-const semPares=[]
-
-
-
 let x=0
-while (x<12) {
+while (x<20) {
     x=x+1
-    if (x%2==0) {
+    if (x%2==1) {
         continue
     }
     console.log(x)
-
 }
