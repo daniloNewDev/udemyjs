395de21b7f9442a8ff50f8f133c2674f2d4d9f28
Danilo Leite <danilo.cebolino@gmail.com>
2023-11-10 14:09:48 -0300

    if-else

--- 4-estrut-if-else/script.js
+++ 4-estrut-if-else/script.js
@@ -1,13 +1,21 @@
 const ageDisplay=() => {
-  let name=prompt("Qual o seu nome?");
-  let age=prompt("Qual a sua idade?");
+  let name=prompt("Qual o seu nome?")
+  let age=prompt("Qual a sua idade?")
   return name=="Danilo"&&age==38
     ? (document.getElementById("resultado").innerHTML=`Shalom, ${name}, você tem ${age} de idade!`)
     :(document.getElementById("resultado").innerHTML=`Quem é você, meu truta?`);
-};
+}
 
 //if(name == "Danilo" && age == 38) {
 //   return document.getElementById("resultado").innerHTML = `Shalom, ${name}, você tem ${age} de idade!`
 //} else {
 //    return document.getElementById("resultado").innerHTML = `Quem é você, meu truta?`
 //}
+
+const radar=() => {
+  let velocidade=prompt("Digite a velocidade:")
+
+  velocidade<=60
+    ? document.getElementById("resultado2").innerHTML="Boa viagem!"
+    :document.getElementById("resultado2").innerHTML=`Ops! Foi multado, andando a ${velocidade}Km/h! ${velocidade-60}km/h acima do permitido.`
+}
