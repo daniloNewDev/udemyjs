81a3e0b4c9cf493a72d187a1ea06bf39c6468517
Danilo Leite <danilo.cebolino@gmail.com>
2023-11-27 11:34:33 -0300

    regex

--- 03-regex/02-operadores/script.js
+++ 03-regex/02-operadores/script.js
@@ -19,12 +19,56 @@ console.log(umOumais.test("asgahath"))
 console.log(umOumais.test(" "))
 console.log(umOumais.test(" feg4eryj"))
 
+//  Question    //
 const padrao = /123?5/
 
 console.log(padrao.test("53235"))
 console.log(padrao.test("1235"))
 
+//  Ocorrências Precisas    //
 const fone = /\(\d{2}\)\d{5}-\d{4}/
 
 console.log(fone.test("(11)98888-4444"))
 console.log(fone.test("11988884444"))
+
+//  Método Exec //
+const digit = /\d+/
+
+console.log(digit.exec("Tem o número 100 aqui"))
+console.log(digit.exec("Aqui não"))
+
+//  Método Match    //
+const frase = "Número 100 está aqui!".match(/\d+/)
+
+console.log(frase)
+
+//  Choice Pattern  //
+const regChoice = /\w+: ("Moshe|Danilo|Sara)/
+
+console.log(regChoice.test("Nome: Danilo"))
+console.log(regChoice.test("Nome: Amanda"))
+console.log(regChoice.test("Nome: Sara"))
+
+//  Validando um domínio:
+const validaDominio = /[?www].\w+\.com.br|.com/
+
+console.log(validaDominio.test("www.google.com"))
+console.log(validaDominio.test("www.maincode.com.br"))
+
+//  Validando um e-mail:
+const validaEmail = /\w+@\w+\.\w+/
+
+console.log(validaEmail.test("dannewdev@gmail.com"))
+console.log(validaEmail.test("dannewdev@gmail"))
+console.log(validaEmail.test("gmail.com"))
+console.log(validaEmail.test("@gmail.com"))
+
+//  Validando data de nascimento:
+const validaData = /[0-9]{2}[/][0-9]{2}[/][0-9]{4}/
+
+console.log(validaData.test("13/08/1985"))
+console.log(validaData.test("1/1/2001"))
+console.log(validaData.test("1/01/2001"))
+console.log(validaData.test("1/01/91"))
+console.log(validaData.test("31-01-2001"))
+
---
+++ 03-regex/03-exercicios/script.js
@@ -0,0 +1,6 @@
+//  Letras maiúsculas
+
+const onlyUpper = /[A_Z]/
+console.log(onlyUpper.test("SHALOM"))
+console.log(onlyUpper.test("Shalom!"))
+console.log(onlyUpper.test("MANISHMAH!!!"))
